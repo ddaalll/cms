392c448973dd9170726605bab972e019f30b173e
NAM <hbkt1857@gmail.com>
2023-10-25 23:30:42 +0900

    feat: [003-EMAIL-VERIFICATION] 커스터머 이메일 verification.

---
+++ src/main/java/com/zerobase/cms/application/SignUpApplication.java
@@ -0,0 +1,59 @@
+package com.zerobase.cms.application;
+
+import com.zerobase.cms.client.MailgunClient;
+import com.zerobase.cms.client.mailgun.SendMailForm;
+import com.zerobase.cms.domain.SignUpForm;
+import com.zerobase.cms.domain.model.Customer;
+import com.zerobase.cms.exception.CustomException;
+import com.zerobase.cms.exception.ErrorCode;
+import com.zerobase.cms.service.SignUpCustomerService;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpApplication {
+    private final MailgunClient mailgunClient;
+    private final SignUpCustomerService signUpCustomerService;
+
+    public void customerVerify(String email, String code){
+        signUpCustomerService.verifyEmail(email, code);
+    }
+
+    public String customerSignUp(SignUpForm form) {
+        if (signUpCustomerService.isEmailExist(form.getEmail())) {
+            throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
+        } else {
+            Customer c = signUpCustomerService.signUp(form);
+
+            String code = getRandomCode();
+            SendMailForm sendMailForm = SendMailForm.builder()
+                    .from("hbkt1857@gmail.com")
+                    .to(form.getEmail())
+                    .subject("Verification Email!")
+                    .text(getVerificationEmailBody(c.getEmail(), c.getName(), getRandomCode()))
+                    .build();
+            mailgunClient.sendEmail(sendMailForm);
+            signUpCustomerService.ChangeCustomerValidateEmail(c.getId(), code);
+            return "회원가입에 성공하였습니다.";
+        }
+    }
+
+    private String getRandomCode() {
+        return RandomStringUtils.random(10, true, true);
+    }
+
+    private String getVerificationEmailBody(String email, String name, String code) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Hello").append(name).append("! Please Click Link for verification. \n\n")
+                .append("http://localhost:8081/signup/verify/customer?email=")
+                .append(email)
+                .append("&code=")
+                .append(code).toString();
+    }
+
+}
